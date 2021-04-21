5bdc0310fd4a7df1ae01d7787a56739caf45943e
Jabbers <Joeylbyrne@gmail.com>
2021-04-21 20:07:39 +0100

    update to version 1.1

--- youtubevideosearch.js
+++ youtubevideosearch.js
@@ -1,32 +1,31 @@
-var toggle = true;
-chrome.storage.sync.get(['enable_youtube_video_search'], function(result) {
-	toggle = result.enable_youtube_video_search;
-	console.log(toggle);
-});
+
 $(window).on('load', function() {
+	var toggle = false;
+	chrome.storage.sync.get(['enable_youtube_video_search'], function(result) {
+		toggle = result.enable_youtube_video_search;
+		if (toggle == true) {
+			var searchQ = "";
+			// get search text
+			var searchText = document.getElementsByTagName('input')
+			for(var i=0;i<searchText.length;i++) {
+				if (searchText[i].getAttribute("title") == "Search") {
+					searchQ = searchText[i].value;
+					break;
+				}
+			}	
+			
 
-	if (toggle == true) {
-		var searchQ = "";
-		// get search text
-		var searchText = document.getElementsByTagName('input')
-		for(var i=0;i<searchText.length;i++) {
-			if (searchText[i].getAttribute("title") == "Search") {
-				searchQ = searchText[i].value;
-				break;
-			}
-		}	
-		
 
-
-		// get video tab
-		var videoTab = document.getElementsByTagName('a')
-		for(var i=0;i<videoTab.length;i++) {
-			if (videoTab[i].innerHTML.toLowerCase().includes("videos")) {
-				var videos = videoTab[i];
-				// set href hyperlink to youtube search query link
-				videos.setAttribute("href", "https://www.youtube.com/results?search_query="+searchQ);
-				break;
+			// get video tab
+			var videoTab = document.getElementsByTagName('a')
+			for(var i=0;i<videoTab.length;i++) {
+				if (videoTab[i].innerHTML.toLowerCase().includes("videos")) {
+					var videos = videoTab[i];
+					// set href hyperlink to youtube search query link
+					videos.setAttribute("href", "https://www.youtube.com/results?search_query="+searchQ);
+					break;
+				}
 			}
 		}
-	}
+	});
 });
